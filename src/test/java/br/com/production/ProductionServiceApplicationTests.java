8eef150ffac3b930a765cdd1a1e8a2dd7d04ed0e
Gustavo Biasi <>
2025-04-30 17:46:42 -0300

    first commit

---
+++ src/test/java/br/com/production/ProductionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.production;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
